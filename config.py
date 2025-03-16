039e7b3cae3e7171e1b6f679ea0d9ece14b3e8d6
RiyoGod <godsxrio@gmail.com>
2025-03-16 23:13:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    API_ID = 
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = 26416419
+    API_HASH = "c109c77f5823c847b1aeb7fbd4990cc4"
+    TOKEN = "7670291188:AAEIyqhshuMMdxgZfH8BChChtn7p6zNm6J8"  
+    START_PIC = "https://i.ibb.co/KpLqV3mP/photo-2025-03-16-17-43-15-7482464389824512012.jpg" 
+    CHAT = "beakatsuki"    

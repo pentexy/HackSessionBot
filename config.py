5b6530da6095bc1a059e7c8179c9d85f519b22d6
𝐏 𝐄 𝐍 𝐓 𝐄 𝐗 𝐘 <godsxrio@gmail.com>
2025-05-06 11:49:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
     API_ID = 26416419
     API_HASH = "c109c77f5823c847b1aeb7fbd4990cc4"
-    TOKEN = "7670291188:AAG1vAULRdIFA6yTXy7LKiLyo2oO1lA1QdA"  
+    TOKEN = "7719581950:AAGDgDzPcYK0xLvT2XNau2k41zmpgVLD368"  
     START_PIC = "https://i.ibb.co/KpLqV3mP/photo-2025-03-16-17-43-15-7482464389824512012.jpg" 
     CHAT = "beakatsuki"    
